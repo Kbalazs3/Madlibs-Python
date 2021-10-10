168779c5826d01e602f3149275caf77aedad7422
Kbalazs3 <kbalazs93@gmail.com>
2021-10-10 16:34:50 +0200

    Functions are started

---
+++ Madlibs.py
@@ -0,0 +1,25 @@
+# It is a Madlibs game written inPython.
+
+
+def stat_menu():
+    pass
+
+
+def align_to_middle():
+    return "\t" * 6
+
+
+def game_modes():
+    return "\tAnimals\n\tFamous People\n\t"
+
+
+def ask_game_mode():
+    game_mode = input(align_to_middle() + "Choose the type of Madlibs Game, You want to play: \n")
+    
+
+def game_logic():
+    pass
+
+
+if __name__ == '__main__':
+    stat_menu()
